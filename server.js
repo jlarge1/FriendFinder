ca84a0d07f2d5a42c1281238ba56575c3d387da0
jlarge1 <largejonathan@gmail.com>
2018-07-24 18:33:41 -0400

    initial files

---
+++ server.js
@@ -0,0 +1,25 @@
+// dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+
+// set up express
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+// set up express to parse data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// router
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
+
+
+// listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
