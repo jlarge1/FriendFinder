32d45016ed79d98cdfb489c8af2869068b1aa1c0
jlarge1 <largejonathan@gmail.com>
2018-07-25 22:44:39 -0400

    cleaned up files

--- server.js
+++ server.js
@@ -2,22 +2,17 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-
-
 // set up express
 var app = express();
 var PORT = process.env.PORT || 8080;
-
 
 // set up express to parse data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 // router
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
-
 
 // listener
 app.listen(PORT, function() {
